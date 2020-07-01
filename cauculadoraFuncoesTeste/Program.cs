6851fb0fee615c053e248829bdc538dfdab7e3b0
David <dcicero493@gmail.com>
2020-07-01 19:17:52 -0300

    Primeira urna console

---
+++ cauculadoraFuncoesTeste/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cauculadoraFuncoesTeste
+{
+    class Program
+    {
+        static bool Main(string[] args)
+        {
+            //URNA ELETRONICA.
+            //criar as variaveis, criar um laço de repetição para que outros eleitores possam votar
+            //criar condições para que os votos vao para os candidatos
+            //perguntar se vai querer encerar votação e mostrar os votos.
+
+            bool continuar = true;
+            int cand1 = 0;
+            int cand2 = 0;
+            int cand3 = 0;
+
+            while (continuar)
+            {
+                Console.WriteLine("ESCOLHA SEU CANDIDATO");
+                Console.WriteLine("JOÃO");
+                Console.WriteLine("CALY");
+                Console.WriteLine("FERNANDA");
+                string voto = Console.ReadLine();
+
+                if (voto == "JOÃO")
+                    cand1++;
+                else if (voto == "CARLY")
+                    cand2++;
+                else if (voto == "FERNANDA")
+                    cand3++;
+                else
+                    Console.WriteLine("VOTO INVALIDO");
+
+                Console.WriteLine("ERNCERRAR VOTAÇÃO");
+                string encerrar = Console.ReadLine();
+
+                if (encerrar == "S")
+                    continuar = false;
+
+            }
+            Console.WriteLine("RESULTADO DA VOTAÇÃO");
+            Console.WriteLine("JOÃO " +cand1);
+            Console.WriteLine("CARLY " +cand2);
+            Console.WriteLine("FERNANDA " +cand3);
+
+
+    }   }
+}
